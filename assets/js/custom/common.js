bb9294715a45d9ba14e8e8d001575f91132e6164
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-11-02 17:04:37 +0600

    accordion updated

--- assets/js/custom/common.js
+++ assets/js/custom/common.js
@@ -116,4 +116,29 @@ $(document).ready(function () {
   // location ended
 });
 
+// all Locationfilter started
+let list = document.querySelectorAll(".list");
+let itemBox = document.querySelectorAll(".item");
 
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active");
+    }
+    this.classList.add("active");
+
+    let dataFilter = this.getAttribute("data-filter");
+    for (let k = 0; k < itemBox.length; k++) {
+      itemBox[k].classList.remove("active");
+      itemBox[k].classList.add("hide");
+
+      if (
+        itemBox[k].getAttribute("data-item") == dataFilter ||
+        dataFilter == "usa"
+      ) {
+        itemBox[k].classList.remove("hide");
+        itemBox[k].classList.add("active");
+      }
+    }
+  });
+}
