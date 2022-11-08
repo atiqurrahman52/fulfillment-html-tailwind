87827b9f5639bb7d9fc666fda52ca01751cd09a6
atiqurrahman52 <atiqur.rahman@intelli.global>
2022-11-08 12:20:54 +0600

    about page bug fix

--- assets/js/custom/common.js
+++ assets/js/custom/common.js
@@ -37,22 +37,6 @@ $(document).ready(function () {
   });
   // count up ended
 
-  // services started
-
-  $(".acc-container .acc:nth-child(1) .acc-head").addClass("active");
-  $(".acc-container .acc:nth-child(1) .acc-content").slideDown();
-  $(".acc-head").on("click", function () {
-    if ($(this).hasClass("active")) {
-      $(this).siblings(".acc-content").slideUp();
-      $(this).removeClass("active");
-    } else {
-      $(".acc-content").slideUp();
-      $(".acc-head").removeClass("active");
-      $(this).siblings(".acc-content").slideToggle();
-      $(this).toggleClass("active");
-    }
-  });
-
   // brand logos started
   $(".brand-logos").slick({
     dots: false,
